bf7661ea2874eafc183a9d3c640cc45c7cce1416
TaniaMed <ikrendelek@mail.ru>
2017-06-29 12:22:23 +0300

    3

--- shop/js/cart.js
+++ shop/js/cart.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function clickProduct(productNode) {
     productNode.addEventListener('click', function(event) {
         addToCart({
@@ -13,4 +13,23 @@ function clickProduct(productNode) {
 Array
     .from(document.getElementsByClassName('add-to-cart'))
     .forEach(clickProduct);
-        
+    
+*/    
+
+
+const itemsList = document.querySelector('.items-list');
+itemsList.addEventListener('click', clickItemsList);
+
+function clickItemsList(event) {
+    Array
+    .from(document.getElementsByClassName('add-to-cart'))
+    .forEach(function(cart) {
+        if (cart === event.target) {
+            addToCart({
+                    title : event.target.getAttribute('data-title'),
+                    price: event.target.getAttribute('data-price')
+                  }) 
+            event.preventDefault();
+        }
+    });  
+}
